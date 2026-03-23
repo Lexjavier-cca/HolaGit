a302b4d80351de1bd3ddf454513590f9b2de80d8
Lexjavi <caizacalispa@gmail.com>
2026-03-23 15:52:42 -0500

    Añadiendo un 2 para probar el local y remoto en hola1

--- hola1.java
+++ hola1.java
@@ -1,5 +1,5 @@
 //Hola 
 public class hola1 {
     public static void main(String[] args) {    
-        System.out.println("probando el local y remoto de github");
+        System.out.println("probando el local y remoto de github2");
 System.out.println("hola");}}
